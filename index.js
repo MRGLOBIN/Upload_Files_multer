4b7dd306acd6ea4d3a587d51d257dd473eb22918
Muhammad Asim <mrgoblin@Muhammads-MacBook-Pro.local>
2023-07-29 21:57:08 +0500

    video streaming complete

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express')
 const path = require('path')
+const fs = require('fs')
+
+const express = require('express')
 const multer = require('multer')
 
-const PROT = 6000
+const PORT = 8000
 
 const app = express()
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+  destination: function (req, file, cb) {
     return cb(null, './uploads')
   },
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}-${file.filename}`)
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`)
   },
 })
 
 const upload = multer({ storage })
 
-app.set('view engine', ejs)
-app.set('view', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -32,6 +34,34 @@ app.post('/uploads', upload.single('profileImage'), (req, res) => {
   console.log(req.file)
 
   return res.redirect('/')
+})
+
+app.get('/video', (req, res) => {
+  const range = req.headers.range
+  if (!range) {
+    res.status(400).send('Requires Range header')
+  }
+
+  const vdieoPath = './uploads/hello.mp4'
+  const videoSize = fs.statSync(vdieoPath).size
+
+  const CHUNK_SIZE = 10 ** 6 // means 10 power 6 (1 MB)
+  const start = Number(range.replace(/\D/g, ''))
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+
+  const length = end - start + 1
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': length,
+    'Content-Type': 'vidoe/mp4',
+  }
+
+  res.writeHead(206, headers)
+
+  const videoStream = fs.createReadStream(vdieoPath, { start, end })
+
+  videoStream.pipe(res)
 })
 
 app.listen(PORT, () => {
